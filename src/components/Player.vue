f3097596dd6d50725320d7b398d8ee67e55cc37e
Сергей Скороход <serik.plus.plus@gmail.com>
2025-02-28 22:13:37 +0300

    feat: пробная текстура игрока

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -14,10 +14,13 @@ const props  = defineProps<{ position: Positions }>();
 const playerStyle = computed<CSSProperties>(() => ({
   position: 'absolute',
   width: '50px',
-  height: '50px',
-  backgroundColor: 'blue',
-  left: `${props.position.x * 50}px`,
-  top: `${props.position.y * 50}px`,
+  height: '65px',
+  backgroundImage: 'url(/Idle.png)',
+  backgroundSize: '50px 65px',
+  backgroundPosition: '0 0',
+
+  left: `${props.position.x * 80}px`,
+  top: `${props.position.y * 80}px`,
   transition: 'all 0.2s ease',
 }));
 </script>
